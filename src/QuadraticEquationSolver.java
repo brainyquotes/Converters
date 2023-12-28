fad17e9f1ae5e81f484c45cd824b3ddaa6ebd925
emjay-tech <142721665+emjay-tech@users.noreply.github.com>
2023-12-28 07:55:05 -0800

    Quadratic Equation Solver

--- src/QuadraticEquationSolver.java
+++ src/QuadraticEquationSolver.java
@@ -1,15 +1,19 @@
 public class QuadraticEquationSolver {
-    public double discriminant(double a, double b, double c) {
-        return b * b - 4 * a * c;
+    private int a, b, c;
+
+    public void setVariables(int coefficientA, int coefficientB, int coefficientC){
+        a = coefficientA;
+        b = coefficientB;
+        c = coefficientC;
     }
 
-    public void solve(double a, double b, double c) {
-        double d = discriminant(a, b, c);
-        if (d < 0) { //this can be used to determine complex roots
+    public void getResult() {
+        double discriminant = (b * b) - (4 * a * c);
+        if (discriminant < 0) {
             System.out.println("No roots or Complex roots");
-        } else if (d > 0) {
-            double x1 = (-b + Math.sqrt(d)) / (2.0 * a);
-            double x2 = (-b - Math.sqrt(d)) / (2.0 * a);
+        } else if (discriminant > 0) {
+            double x1 = (-b + Math.sqrt(discriminant)) / (2.0 * a);
+            double x2 = (-b - Math.sqrt(discriminant)) / (2.0 * a);
             System.out.println("x1 = " + x1 + ", x2 = " + x2);
         } else {
             System.out.println("x = " + ((-b) / (2.0 * a)));
