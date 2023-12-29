da5bed73e1e66841cbfc80ed45de64d617b96a74
emjay-tech <142721665+emjay-tech@users.noreply.github.com>
2023-12-28 19:16:32 -0800

    Quadratic Equation Solver

--- src/QuadraticEquationSolver.java
+++ src/QuadraticEquationSolver.java
@@ -9,13 +9,13 @@ public class QuadraticEquationSolver {
 
     public void getResult() {
         int discriminant = (b * b) - (4 * a * c);
-        if (discriminant < 0) {
-            System.out.println("No roots or Complex roots");
-        } else if (discriminant > 0) {
+        if (discriminant < 0) { //the roots are not real
+            System.out.println("Complex roots");
+        } else if (discriminant > 0) {// the roots are two distinct values
             double x1 = (-b + Math.sqrt(discriminant)) / (2.0 * a);
             double x2 = (-b - Math.sqrt(discriminant)) / (2.0 * a);
             System.out.println("x1 = " + x1 + ", x2 = " + x2);
-        } else {
+        } else { //one real root
             System.out.println("x = " + ((-b) / (2.0 * a)));
         }
     }
